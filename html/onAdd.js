19b21c4a074697362d025e21357014682f504a99
ilya-beginner <ilya.beginner@gmail.com>
2024-08-08 21:05:46 +0300

    2

--- html/onAdd.js
+++ html/onAdd.js
@@ -2,6 +2,7 @@ async function onAdd() {
     date = document.getElementById('newDate').value;
     type = document.getElementById('newType').value;
     sum = parseFloat(document.getElementById('newSum').value.replace(',','.'));
+    alert(sum);
     currency = document.getElementById('newCurrency').value;
     tag = document.getElementById('newTag').value;
     notes = document.getElementById('newNotes').value;
